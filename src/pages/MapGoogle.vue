63ea9eff12b5767c232abeffe687bfaf1ddfb679
Alireza Shakouri <alirezasha01@gmail.com>
2023-01-03 21:30:47 +0330

    Complete the map component

--- src/pages/MapGoogle.vue
+++ src/pages/MapGoogle.vue
@@ -10,6 +10,7 @@
       <div class="mb-5 padding-main">
         <p class="address-color px-sm-4 px-3 py-sm-2 pt-3">موقعیت مورد نظر خود را روی نقشه مشخص کنید</p>
         <div class="px-4 pb-4">
+          <div id="map"></div>
         </div>
       </div>
     </div>
@@ -22,12 +23,51 @@
 
 <script>
 import Button from "../components/Button.vue"
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 export default {
   components: {
     Button
   },
   name: "GoogleMap",
+  data() {
+    return {
+      map: null,
+      point: {
+        lat: null,
+        lng: null
+      },
+      marker: null,
+      iconMarker: null
+    };
+  },
+  mounted() {
+    let map = L.map('map').setView([35.7036534, 51.3495146], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    map.on('click', function (e) {
+      this.lat = e.latlng.lat;
+      this.lng = e.latlng.lng;
+      if (this.marker !== undefined) {
+        map.removeLayer(this.marker);
+      }
+      this.iconMarker = L.icon({
+        iconUrl: "https://img.icons8.com/ios-filled/50/null/marker.png",
+        iconSize: [50, 50],
+        shadowSize: [200, 200],
+      }),
+          this.marker = new L.Marker(e.latlng, {icon: this.iconMarker, draggable: true});
+      map.addLayer(this.marker);
+      this.marker.bindPopup("مکان شما").openPopup();
+      console.log(this.lat, this.lng)
+    });
+
+  },
+  beforeUnmount() {
+    if (this.map) {
+      this.map.remove();
+    }
+  }
 }
 </script>
 
@@ -67,4 +107,8 @@ label {
   text-decoration: none;
 }
 
+#map {
+  width: 100%;
+  height: 300px;
+}
 </style>
